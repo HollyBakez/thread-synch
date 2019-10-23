f8f8735990e692639c4b895b5e2873b30dfb9756
HollyBakez <ho.holland1999@gmail.com>
2019-10-23 14:49:43 -0700

    working TODO ThreadB

--- synchronization.cpp
+++ synchronization.cpp
@@ -12,35 +12,51 @@ using namespace std;
 
 
 
-/* thread function to write */
+/* thread function to write 1-26 */
 void* WriteThreadA(void* arg){
-    string *file_input = (string*) arg;
-    cout << "Hello World \n";
+    fstream *file_input = (fstream*) arg;
+    int counter = 0;
+    /* writes numbers 1 - 26 20x to the file*/
+    for(int i = 0; i < 20; i++){
+        for(int j = 0; j < 26; j++){
+            *file_input << j+1 << " " ;
+        }
+        *file_input << endl;
+    }
     pthread_exit(0);
 }
 
+/* thread function to write A-Z */
 void* WriteThreadB(void* arg){
-    string *file_input = (string*) arg;
-    cout << "I love OS! \n";
+    //vector<char> letter; 
+
+    fstream *file_input = (fstream*) arg;
+    for(int i = 0; i < 20; i++){
+        for(int j = 0; j < 26; j++){
+            *file_input << j+1 << " " ;
+        }
+        *file_input << endl;
+    }
     pthread_exit(0);
 }
 int main(){
     /* thread id */
-    pthread_t tid;
+    pthread_t tid1, tid2;
 
     /* create attributes */
     pthread_attr_t attr;
     pthread_attr_init(&attr);
 
     /* create synch.txt file */
-    ofstream output_file;
+    fstream output_file;
     output_file.open("synch.txt");
-
     /* pass txt file by creating a thread and waits until the thread has done its work */
-    pthread_create(&tid, &attr, WriteThreadA, &output_file);
-    pthread_create(&tid, &attr, WriteThreadB, &output_file);
-    pthread_join(tid, NULL);
-    //pthread_create(&tid, &attr, WriteThreadB, &output_file);
+    pthread_create(&tid1, &attr, WriteThreadA, &output_file);
+    pthread_join(tid1, NULL);
+
+    pthread_create(&tid2, &attr, WriteThreadB, &output_file);
+    pthread_join(tid2, NULL);
+
 
     return 0 ;
 }
