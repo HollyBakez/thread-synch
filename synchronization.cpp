8251602de61619aaae22a9be0d1ffbd619b9be53
HollyBakez <ho.holland1999@gmail.com>
2019-10-23 13:00:54 -0700

    passes txt in successfully

--- synchronization.cpp
+++ synchronization.cpp
@@ -2,19 +2,28 @@
 // Copyright(c) Holland Ho 10/22/19
 //libraries for thread and file input/output
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <fstream>
+#include <mutex>
 
 using namespace std;
 
-/* thread function to write */
-void* WriteThreadA(void* file_arg){
 
-    for(int i = 0; i < 26; i++){
-        
-    }
+
+/* thread function to write */
+void* WriteThreadA(void* arg){
+    string *file_input = (string*) arg;
+    cout << "Hello World \n";
+    pthread_exit(0);
 }
 
+void* WriteThreadB(void* arg){
+    string *file_input = (string*) arg;
+    cout << "I love OS! \n";
+    pthread_exit(0);
+}
 int main(){
     /* thread id */
     pthread_t tid;
@@ -23,11 +32,15 @@ int main(){
     pthread_attr_t attr;
     pthread_attr_init(&attr);
 
-    fstream file;
-    file.open("synch.txt");
-    file << "hello world";
+    /* create synch.txt file */
+    ofstream output_file;
+    output_file.open("synch.txt");
 
-    //pthread_create(&tid, &attr, WriteThreadA, file);
+    /* pass txt file by creating a thread and waits until the thread has done its work */
+    pthread_create(&tid, &attr, WriteThreadA, &output_file);
+    pthread_create(&tid, &attr, WriteThreadB, &output_file);
+    pthread_join(tid, NULL);
+    //pthread_create(&tid, &attr, WriteThreadB, &output_file);
 
     return 0 ;
 }
